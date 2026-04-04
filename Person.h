c75c70748aa68cb1007b3b1049e1a566cbc649fd
Madison Schultz <167554381+madischu@users.noreply.github.com>
2026-04-03 23:07:51 -0400

    Add Person struct to store person information

---
+++ Person.h
@@ -0,0 +1,14 @@
+#ifndef PERSON_H
+#define PERSON_H
+
+#include <string>
+using namespace std;
+
+// Struct that stores information about a person in line
+struct Person
+{
+    string name;
+    int numberInLine;
+};
+
+#endif
